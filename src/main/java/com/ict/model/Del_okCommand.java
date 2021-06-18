cf91b0b7ce58419241637c557af1f5c258275fec
choi970702 <choi970702@gmail.com>
2021-06-18 09:30:20 +0900

    1. 21/06/18

---
+++ src/main/java/com/ict/model/Del_okCommand.java
@@ -0,0 +1,22 @@
+package com.ict.model;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ict.db.DAO;
+
+public class Del_okCommand implements Command
+{
+	@Override
+	public String exec(HttpServletRequest request, HttpServletResponse response)
+	{
+		String idx = request.getParameter("idx");
+		int result = DAO.getInstance().getDelete(idx);
+		if(result>0)
+		{
+			//response.sendRedirect("list.jsp");
+			return "MyController?cmd=list";
+		}
+		return null;
+	}
+}
